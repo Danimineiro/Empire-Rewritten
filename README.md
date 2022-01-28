bc5f505a270b5b08121a8e6295c180e6dd267f1b
Daniel Plaster <daniel_plaster@live.de>
2022-01-28 01:45:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Empire-Rewritten
+ This is the project in which we intent to rewrite the RimWorld mod Empire in
---
+++ TestClass.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace Empire_Rewritten
+{
+    [StaticConstructorOnStartup]
+    public class TestClass
+    {
+        static TestClass()
+        {
+            Log.Message($"<color=orange>[Empire] Test succeeded ^-^</color>");
+        }
+    }
+}
