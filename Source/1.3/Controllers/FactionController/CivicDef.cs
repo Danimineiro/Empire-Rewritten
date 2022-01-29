b35a7d4125abd47034b80d1f254d76ea15e31305
Daniel Plaster <daniel_plaster@live.de>
2022-01-29 23:01:31 +0100

    created CivicDef

---
+++ Source/1.3/Controllers/FactionController/CivicDef.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Empire_Rewritten
+{
+    public class CivicDef : Def
+    {
+        public float StatMultiplier;
+        public float StatOffset;
+    }
+
+    [DefOf]
+    public class CivicDefOf
+    {
+        static CivicDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(CivicDefOf));
+    }
+
+}
