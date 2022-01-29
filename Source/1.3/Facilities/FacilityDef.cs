a22d4e47f9613b335380fe17e80679e22a026d5a
NesiAvali <fiveggamer@gmail.com>
2022-01-29 15:11:22 -0500

    Facilites, Resources, and Settlements
    Added Facilities, FacilitiyDefs, the FacilitiyManager, a basic ResourceDef and ResourceModifier, and the basic SettlementManager.

---
+++ Source/1.3/Facilities/FacilityDef.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Empire_Rewritten
+{
+    public class FacilityDef : Def
+    {
+        public Dictionary<ResourceDef, float> resourceMultipliers = new Dictionary<ResourceDef, float>();
+
+        public float GetResourceModifier(ResourceDef resourceDef)
+        {
+            if (resourceMultipliers.ContainsKey(resourceDef))
+            {
+                return resourceMultipliers[resourceDef];
+            }
+            //No effect on the resource
+            return 1f;
+        }
+
+
+
+    }
+}
---
+++ Source/1.3/Settlements/SettlementManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld.Planet;
+using Verse;
+
+namespace Empire_Rewritten
+{
+    /// <summary>
+    /// Manages settlements.
+    /// </summary>
+    public class SettlementManager : IExposable, ILoadReferenceable
+    {
+
+        private Dictionary<Settlement, FacilityManager> settlements = new Dictionary<Settlement, FacilityManager>();
+
+
+        public Dictionary<Settlement,FacilityManager> Settlements
+        {
+            get { return settlements; }
+        }
+
+        public void AddSettlement(Settlement settlement)
+        {
+            FacilityManager tracker = new FacilityManager(settlement);
+            settlements.Add(settlement, tracker);
+        }
+
+        public void ExposeData()
+        {
+            Scribe_Collections.Look(ref settlements, "settlements", LookMode.Undefined, LookMode.Deep);
+
+        }
+
+        public FacilityManager GetFacilityTracker(Settlement settlement)
+        {
+            if (settlements.ContainsKey(settlement))
+            {
+                return settlements[settlement];
+            }
+            Log.Warning($"[Empire]: {settlement.Name} was not in the settlement manager! Returning null.");
+            return null;
+        }
+
+        public string GetUniqueLoadID()
+        {
+            return $"SettlementTracker_{GetHashCode()}";
+        }
+    }
+}
