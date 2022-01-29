28c9fa9111d5e8f6fcf2a8c430b5bfbe6e9ceb62
Daniel Plaster <daniel_plaster@live.de>
2022-01-30 00:40:52 +0100

    updated the FacilityDef class to contain ModChecks

--- Source/1.3/Facilities/FacilityDef.cs
+++ Source/1.3/Facilities/FacilityDef.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Empire_Rewritten.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,16 @@ namespace Empire_Rewritten
         public Dictionary<ResourceDef, float> resourceMultipliers = new Dictionary<ResourceDef, float>();
         public Dictionary<ResourceDef,int> resourceOffsets = new Dictionary<ResourceDef, int>();
 
+        public readonly bool requiresIdeology = false;
+        public readonly bool requiresRoyality = false;
+        public readonly List<string> requiredModIDs = new List<string>();
 
         public Type facilityWorker;
+
+        /// <summary>
+        /// Returns if all required Mods are loaded
+        /// </summary>
+        public bool RequiredModsLoaded => ModChecker.RequiredModsLoaded(requiredModIDs, requiresRoyality, requiresIdeology);
 
         public override IEnumerable<string> ConfigErrors()
         {
---
+++ Source/1.3/Utils/ModChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Empire_Rewritten.Utils
+{
+    public static class ModChecker
+    {
+        public static bool AreModsLoaded(List<string> ModIDs) => ModIDs.TrueForAll(mod => ModsConfig.IsActive(mod));
+
+        public static bool RequiredModsLoaded(List<string> requiredModIDs, bool requiresRoyality = false, bool requiresIdeology = false)
+        {
+            return (ModsConfig.RoyaltyActive || !requiresRoyality) && (ModsConfig.IdeologyActive || !requiresIdeology) && AreModsLoaded(requiredModIDs);
+        }
+    }
+}
