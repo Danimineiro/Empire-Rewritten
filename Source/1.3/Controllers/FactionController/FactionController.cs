ab0571a43c6ec7cf733b4824570b0736d8e1bc8f
Daniel Plaster <daniel_plaster@live.de>
2022-01-29 22:31:04 +0100

    added empty constructors for saving/Loading

--- Source/1.3/Controllers/FactionController/FactionController.cs
+++ Source/1.3/Controllers/FactionController/FactionController.cs
@@ -10,6 +10,15 @@ namespace Empire_Rewritten
 {
     public class FactionController : IExposable
     {
+        /// <summary>
+        /// Needed for loading
+        /// </summary>
+        public FactionController() { }
+
+        /// <summary>
+        /// Creates a new FactionController using a List of <c>FactionSettlementData</c> structs
+        /// </summary>
+        /// <param name="factionSettlementDataList"></param>
         public FactionController(List<FactionSettlementData> factionSettlementDataList)
         {
             this.factionSettlementDataList = factionSettlementDataList;
@@ -17,9 +26,6 @@ namespace Empire_Rewritten
 
         private List<FactionSettlementData> factionSettlementDataList = new List<FactionSettlementData>();
         
-        /// <summary>
-        /// Saves Data
-        /// </summary>
         public void ExposeData()
         {
             Scribe_Collections.Look(ref factionSettlementDataList, "FactionSettlementDataList");
