df6b706a053133eb59e76dd225401596759bdabe
Daniel Plaster <daniel_plaster@live.de>
2022-01-30 01:04:22 +0100

    added the EthicDef class and changed the CivicDef class

---
+++ Source/1.3/Controllers/FactionController/CivicEthic/EthicDef.cs
@@ -0,0 +1,50 @@
+﻿using Empire_Rewritten.Utils;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Empire_Rewritten
+{
+    public class EthicDef : Def
+    {
+        public List<EmpireStatModifier> statModifiers;
+        public readonly List<Type> abilityWorkers;
+
+        public readonly bool requiresIdeology = false;
+        public readonly bool requiresRoyality = false;
+        public readonly List<string> requiredModIDs = new List<string>();
+
+        /// <summary>
+        /// Returns if all required Mods are loaded
+        /// </summary>
+        public bool RequiredModsLoaded => ModChecker.RequiredModsLoaded(requiredModIDs, requiresRoyality, requiresIdeology);
+
+        public Type facilityWorker;
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            foreach (Type type in abilityWorkers)
+            {
+                if (!type.IsSubclassOf(typeof(EthicDef)))
+                {
+                    yield return $"{type} does not inherit from EthicAbilityWorker!";
+                }
+            }
+
+            foreach (string str in base.ConfigErrors())
+            {
+                yield return str;
+            }
+        }
+    }
+
+    [DefOf]
+    public class EthicDefOf
+    {
+        static EthicDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(EthicDefOf));
+    }
+}
