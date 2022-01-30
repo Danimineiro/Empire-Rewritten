8a931b4dafae221b7666eb5d9cc003db4f95392b
Daniel Plaster <daniel_plaster@live.de>
2022-01-30 01:05:09 +0100

    Added the classes required for Ethics and Civics to work

---
+++ Source/1.3/Controllers/FactionController/CivicEthic/EthicAbilityWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empire_Rewritten
+{
+    public class EthicAbilityWorker
+    {
+        public virtual string GetActionNameTranslationKey()
+        {
+            return "Empire_UnimplementedEthicActionName";
+        }
+
+        public virtual string GetActionDescriptionTranslationKey()
+        {
+            return "Empire_UnimplementedEthicActionDescription";
+        }
+
+        public virtual Action<SettlementManager> ExecutedAction()
+        {
+            return null;
+        }
+    }
+}
