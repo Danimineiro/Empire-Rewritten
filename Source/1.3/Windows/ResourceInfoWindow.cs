515b40a0de9ca21d3f0433117658f5366c359075
Daniel Plaster <daniel_plaster@live.de>
2022-02-01 12:13:25 +0100

    fixed some naming

--- Source/1.3/Windows/ResourceInfoWindow.cs
+++ Source/1.3/Windows/ResourceInfoWindow.cs
@@ -17,57 +17,62 @@ namespace Empire_Rewritten
 
     public class ResourceInfoWindow : Window
     {
-        private readonly static Rect Rect_DefName = new Rect(90f, 20f, 500f, 64f);
-        private readonly static Rect Rect_DefIcon = new Rect(10f, 20f, 64f, 64f);
-        private readonly static Rect Rect_ThingDefContainer = new Rect(610f, 20f, 569f, 40f);
-        private readonly static Rect Rect_ThingDefIcons = new Rect(0f, 0f, 25f, 25f);
-        private readonly static Rect Rect_ThingDefs = new Rect(45f, 0f, 515f, 25f);
-        private readonly static Rect Rect_ThingDefsHighlight = new Rect(0f, 0f, 515f, 25f);
-        private readonly static Rect Rect_TempCurve = new Rect(10f, 400f, 580f, 190f);
-        private readonly static Rect Rect_HeightCurve = new Rect(610f, 400f, 580f, 190f);
-        private readonly static Rect Rect_DefDesc = new Rect(10f, 145f, 580f, 240f);
+        private readonly static Rect rect_DefName = new Rect(90f, 20f, 500f, 64f);
+        private readonly static Rect rect_DefIcon = new Rect(10f, 20f, 64f, 64f);
+        private readonly static Rect rect_ThingDefContainer = new Rect(610f, 20f, 569f, 40f);
+        private readonly static Rect rect_ThingDefIcons = new Rect(0f, 0f, 25f, 25f);
+        private readonly static Rect rect_ThingDefs = new Rect(45f, 0f, 515f, 25f);
+        private readonly static Rect rect_ThingDefsHighlight = new Rect(0f, 0f, 515f, 25f);
+        private readonly static Rect rect_TempCurve = new Rect(10f, 400f, 580f, 190f);
+        private readonly static Rect rect_HeightCurve = new Rect(610f, 400f, 580f, 190f);
+        private readonly static Rect rect_DefDesc = new Rect(10f, 145f, 580f, 240f);
 
-        private readonly static Rect Rect_ScrollRect = new Rect(new Vector2(610f, 20f), new Vector2(580f, 320f));
-        private readonly static Rect Rect_ScrollViewRect = new Rect(new Vector2(610f, 20f), new Vector2(563f, 320f));
+        private readonly static Rect rect_ScrollRect = new Rect(new Vector2(610f, 20f), new Vector2(580f, 320f));
+        private readonly static Rect rect_ScrollViewRect = new Rect(new Vector2(610f, 20f), new Vector2(563f, 320f));
+
+        private readonly static Color gray = new Color(0f, 0f, 0f, 0.5f);
 
         private List<ResourceDef> resources = null;
-        private Vector2 DefDescScrollVector = new Vector2();
-        private Vector2 ScrollRectVector = new Vector2();
+        private Vector2 defDescScrollVector = new Vector2();
+        private Vector2 scrollRectVector = new Vector2();
 
-        private ResourceDef def;
+        private ResourceDef defSelected;
 
         public override Vector2 InitialSize => new Vector2(1240, 640);
 
         public override void DoWindowContents(Rect inRect)
         {
-            DrawSelectorButton();
+            DrawDefSelectorButton();
+            if (defSelected == null) return;
+
             DrawDefInfo();
             DrawItems();
-
-            if (def != null)
-            {
-                SimpleCurveDrawer.DrawCurve(Rect_HeightCurve, def.heightCurve);
-                SimpleCurveDrawer.DrawCurve(Rect_TempCurve, def.temperatureCurve);
-            }
-
+            DrawCurves();
             DrawBoxes();
+        }
+
+        private void DrawCurves()
+        {
+            SimpleCurveDrawer.DrawCurve(rect_HeightCurve, defSelected.heightCurve);
+
+            SimpleCurveDrawer.DrawCurve(rect_TempCurve, defSelected.temperatureCurve);
         }
 
         private void DrawItems()
         {
-            if (def != null && def.ResourcesCreated.AllowedThingDefs.ToList() is List<ThingDef> thingDefs)
+            if (defSelected.ResourcesCreated.AllowedThingDefs.ToList() is List<ThingDef> thingDefs)
             {
-                Widgets.BeginScrollView(Rect_ScrollRect, ref ScrollRectVector, Rect_ScrollViewRect);
+                Widgets.BeginScrollView(rect_ScrollRect, ref scrollRectVector, rect_ScrollViewRect);
                 for (int i = 0; i < thingDefs.Count; i++)
                 {
-                    Rect temp = new Rect(Rect_ThingDefContainer);
+                    Rect temp = new Rect(rect_ThingDefContainer);
                     temp.y += temp.height * i;
                     ThingDef current = thingDefs[i];
 
                     GUI.BeginGroup(temp);
-                    Widgets.DrawHighlight(Rect_ThingDefsHighlight);
-                    GUI.DrawTexture(Rect_ThingDefIcons, Widgets.GetIconFor(current));
-                    Widgets.Label(Rect_ThingDefs, current.label);
+                    Widgets.DrawHighlight(rect_ThingDefsHighlight);
+                    GUI.DrawTexture(rect_ThingDefIcons, Widgets.GetIconFor(current));
+                    Widgets.Label(rect_ThingDefs, current.label);
 
                     GUI.EndGroup();
                 }
@@ -78,30 +83,27 @@ namespace Empire_Rewritten
 
         private void DrawBoxes()
         {
-            if (def == null) return;
-            Widgets.DrawHighlight(Rect_DefDesc);
-            Widgets.DrawHighlight(Rect_TempCurve);
-            Widgets.DrawHighlight(Rect_HeightCurve);
+            Widgets.DrawHighlight(rect_DefDesc);
+            Widgets.DrawHighlight(rect_TempCurve);
+            Widgets.DrawHighlight(rect_HeightCurve);
 
-            Widgets.DrawBox(Rect_DefDesc, 2);
-            Widgets.DrawBox(Rect_TempCurve, 2);
-            Widgets.DrawBox(Rect_HeightCurve, 2);
+            Widgets.DrawBox(rect_DefDesc, 2);
+            Widgets.DrawBox(rect_TempCurve, 2);
+            Widgets.DrawBox(rect_HeightCurve, 2);
         }
 
         private void DrawDefInfo()
         {
-            if (def == null) return;
-
-            GUI.DrawTexture(Rect_DefIcon, ContentFinder<Texture2D>.Get(def.iconData.texPath));
-            Widgets.TextAreaScrollable(Rect_DefDesc, def.description, ref DefDescScrollVector, true);
+            GUI.DrawTexture(rect_DefIcon, ContentFinder<Texture2D>.Get(defSelected.iconData.texPath));
+            Widgets.TextAreaScrollable(rect_DefDesc, defSelected.description, ref defDescScrollVector, true);
         }
 
         /// <summary>
         /// Draws a button that allows the user to select a resource to look at
         /// </summary>
-        private void DrawSelectorButton()
+        private void DrawDefSelectorButton()
         {
-            if (Widgets.ButtonText(Rect_DefName, def?.label ?? "Select a resource def to look at"))
+            if (Widgets.ButtonText(rect_DefName, defSelected?.label ?? "Select a resource def to look at"))
             {
                 Find.WindowStack.Add(new FloatMenu(DefOptions()));
             }
@@ -120,7 +122,7 @@ namespace Empire_Rewritten
             {
                 floatMenuOptions.Add(new FloatMenuOption(def.label, delegate
                 {
-                    this.def = def;
+                    this.defSelected = def;
                 }));
             }
 
