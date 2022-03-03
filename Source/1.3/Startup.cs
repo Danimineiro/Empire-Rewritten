fd89addae6cf1ad68fad2316167c46fe539192fc
Tobias Berger <tobi.berger13@gmail.com>
2022-03-03 13:41:30 +0100

    Tidy Startup

--- Source/1.3/Startup.cs
+++ Source/1.3/Startup.cs
@@ -1,43 +1,52 @@
 ﻿using Empire_Rewritten.Controllers;
 using Empire_Rewritten.HarmonyPatches;
 using Empire_Rewritten.Utils.Text;
+using JetBrains.Annotations;
 using Verse;
 
 namespace Empire_Rewritten
 {
     /// <summary>
-    /// The class that should handle all out startup needs
+    ///     The class that should handle all our startup needs
     /// </summary>
     [StaticConstructorOnStartup]
     public static class Startup
     {
         /// <summary>
-        /// Gets called on startup
+        ///     Gets called on startup
         /// </summary>
+        [UsedImplicitly]
         static Startup()
         {
-            Log.Message("[Empire Rewritten] just here to say hello! ^-^ Have a nice day and great fun with Empire!".Rainbowify(' ', 35));
+            Log.Message("[Empire Rewritten] just here to say hello! ^-^ Have a nice day and great fun with Empire!"
+                            .Rainbowify(' ', 35));
             AppendActionsToWorldStart();
             HarmonyPatcher.DoPatches();
         }
 
         /// <summary>
-        /// Appends functions to when the world is finalized
+        ///     Sets up necessary <see cref="System.Action{T}">Actions</see> to be run after world start.
         /// </summary>
         private static void AppendActionsToWorldStart()
         {
-            Log.Message($"<color=orange>[Empire]</color> Attaching actions to world start");
+            Log.Message("<color=orange>[Empire]</color> Attaching actions to world start");
             UpdateController.AddFinalizeInitHook(AddFactionControllerIfMissing);
         }
 
         /// <summary>
-        /// Adds a FactionController to the <c>UpdateController</c> class if one is missing
+        ///     Adds a <see cref="FactionController" /> to the <see cref="UpdateController" /> class, if not already set
         /// </summary>
-        /// <param name="controller"></param>
-        private static void AddFactionControllerIfMissing(FactionController controller)
+        /// <param name="_">
+        ///     The necessary <see cref="FactionController" /> parameter for
+        ///     <see cref="UpdateController.AddFinalizeInitHook" />;
+        ///     Unused here
+        /// </param>
+        private static void AddFactionControllerIfMissing(FactionController _)
         {
             if (UpdateController.CurrentWorldInstance.HasFactionController) return;
-            UpdateController.CurrentWorldInstance.FactionController = new FactionController(FactionSettlementData.CreateFactionSettlementData());
+
+            UpdateController.CurrentWorldInstance.FactionController =
+                new FactionController(FactionSettlementData.CreateFactionSettlementData());
         }
     }
 }
