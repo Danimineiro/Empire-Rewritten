a6eb2c5397e2a67a1dc32ffccf5f431b21c2e078
Danimineiro <daniel_plaster@live.de>
2022-03-03 20:14:18 +0100

    adds a shouldDiscardOverride Func<bool>
    This function overrides the default discarding functionality with a custom method that can be set by a coder

--- Source/1.3/Controllers/UpdateController/UpdateControllerAction.cs
+++ Source/1.3/Controllers/UpdateController/UpdateControllerAction.cs
@@ -6,6 +6,7 @@ namespace Empire_Rewritten.Controllers
     public class UpdateControllerAction
     {
         private readonly Action<FactionController> action;
+        private readonly Func<bool> shouldDiscardOverride;
 
         private readonly int maxExecutions;
 
@@ -13,6 +14,49 @@ namespace Empire_Rewritten.Controllers
         ///     How often this <see cref="UpdateControllerAction" /> has been executed so far.
         /// </summary>
         public int useCounter;
+
+        /// <summary>
+        ///     Constructs a new <see cref="UpdateControllerAction" /> that executes a given <see cref="Action{T}" /> if conditions
+        ///     are met.
+        ///     Will be discarded if <paramref name="shouldDiscardOverride" /> returns true
+        /// </summary>
+        /// <param name="shouldExecute">
+        ///     The function that determines if the <see cref="UpdateControllerAction.action" /> should be
+        ///     executed.
+        /// </param>
+        /// <param name="action">
+        ///     The <see cref="Action{T}" /> to execute. Takes a single <see cref="FactionController" /> as
+        ///     parameter.
+        /// </param>
+        /// <param name="shouldDiscardOverride">
+        ///     Discards this <see cref="UpdateControllerAction" /> if it returns true. Runs every tick!
+        /// </param>
+        public UpdateControllerAction(Action<FactionController> action, Func<bool> shouldExecute, Func<bool> shouldDiscardOverride)
+        {
+            ShouldExecute = shouldExecute;
+            this.action = action;
+            useCounter = 0;
+            this.shouldDiscardOverride = shouldDiscardOverride;
+        }
+
+        /// <summary>
+        ///     Constructs a new <see cref="UpdateControllerAction" /> that controls when a given <paramref name="action" /> should
+        ///     execute
+        ///     Executes whenever <code>Find.TickManager.TicksGame % <paramref name="tickInterval" /> == 0</code>
+        ///     Will be discarded if <paramref name="shouldDiscardOverride" /> returns true
+        /// </summary>
+        /// <param name="tickInterval"></param>
+        /// <param name="action"></param>
+        /// <param name="shouldDiscardOverride">
+        ///     Discards this <see cref="UpdateControllerAction" /> if it returns true. Runs every tick!
+        /// </param>
+        public UpdateControllerAction(Action<FactionController> action, int tickInterval, Func<bool> shouldDiscardOverride)
+        {
+            ShouldExecute = () => Find.TickManager.TicksGame % tickInterval == 0;
+            this.action = action;
+            useCounter = 0;
+            this.shouldDiscardOverride = shouldDiscardOverride;
+        }
 
         /// <summary>
         ///     Constructs a new <see cref="UpdateControllerAction" /> that executes a given <see cref="Action{T}" /> if conditions
@@ -57,8 +101,20 @@ namespace Empire_Rewritten.Controllers
 
         /// <summary>
         ///     Decides if the <seealso cref="UpdateControllerAction.action" /> can be discarded
+        ///     Uses <see cref="UpdateControllerAction.shouldDiscardOverride"/> if available
         /// </summary>
-        private bool ShouldDiscard => maxExecutions > 0 && useCounter >= maxExecutions;
+        private bool ShouldDiscard
+        {
+            get
+            {
+                if (shouldDiscardOverride != null)
+                {
+                    return shouldDiscardOverride();
+                }
+
+                return maxExecutions > 0 && useCounter >= maxExecutions;
+            }
+        }
 
         /// <summary>
         ///     The <see cref="Func{TResult}" /> that determines if the <see cref="UpdateControllerAction.action" /> should be
@@ -91,7 +147,9 @@ namespace Empire_Rewritten.Controllers
 
         public override string ToString()
         {
-            return $"[{GetType().Name}] useCounter: {useCounter}, maxExecutions: {maxExecutions}, has shouldExecute: {ShouldExecute != null}, has action {action != null}";
+            string discardInfo = (shouldDiscardOverride == null) ? (maxExecutions == 0) ? "never discards" : $"maxExecutions: {maxExecutions}" : " uses discard override";
+
+            return $"[{GetType().Name}] useCounter: {useCounter}, {discardInfo}, has shouldExecute: {ShouldExecute != null}, has action {action != null}";
         }
     }
 }
