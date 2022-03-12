49edd7a1d0eb51c0410a79f39fb2c88afa099c35
Tobias Berger <tobi.berger13@gmail.com>
2022-03-12 21:15:42 +0100

    Add Territory owner to World Tile inspect pane

--- Source/1.3/HarmonyPatches/HarmonyPatcher.cs
+++ Source/1.3/HarmonyPatches/HarmonyPatcher.cs
@@ -1,4 +1,5 @@
-﻿using Empire_Rewritten.Utils;
+﻿using System.Reflection;
+using Empire_Rewritten.Utils;
 using HarmonyLib;
 using RimWorld;
 using RimWorld.Planet;
@@ -13,10 +14,16 @@ namespace Empire_Rewritten.HarmonyPatches
         public static void DoPatches()
         {
             Harmony harmony = new Harmony("EmpireRewritten.HarmonyPatches");
+
             harmony.Patch(typeof(Settlement).GetMethod(nameof(Settlement.GetGizmos)), null, new HarmonyMethod(typeof(SettlementGizmoPatch), nameof(SettlementGizmoPatch.GizmoPatch)));
+
             harmony.Patch(typeof(PlaySettings).GetMethod(nameof(PlaySettings.DoPlaySettingsGlobalControls)), null,
                           new HarmonyMethod(typeof(PlaySettingsControlsPatch), nameof(PlaySettingsControlsPatch.Postfix)));
+
             harmony.Patch(typeof(SettleUtility).GetMethod(nameof(SettleUtility.AddNewHome)), null, new HarmonyMethod(typeof(SettleUtilityPatch), nameof(SettleUtilityPatch.Postfix)));
+
+            harmony.Patch(typeof(WorldInspectPane).GetProperty("TileInspectString", BindingFlags.NonPublic | BindingFlags.Instance)?.GetMethod, null,
+                          new HarmonyMethod(typeof(TileInspectStringFaction), nameof(TileInspectStringFaction.AppendFactionToTileInspectString)));
 
             Logger.Log("Patches completed!");
         }
