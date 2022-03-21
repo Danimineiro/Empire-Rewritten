76d62ca77d612c58393df0c516dbdf42e5a50d0e
Tobias Berger <tobi.berger13@gmail.com>
2022-03-21 12:27:59 +0100

    Move Window DebugActions to own file

---
+++ Source/1.3/Windows/DebugActions.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using Verse;
+
+namespace Empire_Rewritten.Windows
+{
+    public static class DebugActions
+    {
+        [PublicAPI]
+        [DebugAction("Empire", "Resource info window", allowedGameStates = AllowedGameStates.Entry)]
+        public static void DisplayResourceInfoWindow()
+        {
+            Find.WindowStack.Add(new ResourceInfoWindow());
+        }
+
+        [PublicAPI]
+        [DebugAction("Empire", "Facility info window", allowedGameStates = AllowedGameStates.Entry)]
+        public static void FacilityInfoWindow()
+        {
+            Find.WindowStack.Add(new FacilityInfoWindow());
+        }
+
+        [PublicAPI]
+        [DebugAction("Empire", "Color picker window", allowedGameStates = AllowedGameStates.Entry)]
+        public static void ColorPickerWindow()
+        {
+            Color color = Color.green;
+            Color[] colors = new Color[10];
+            Find.WindowStack.Add(new ColorPickerWindow(color, colors, (_) => { }, (_) => { }));
+        }
+    }
+}
