4817407393475a985c1255d2615f3e7fcea078b4
Daniel Plaster <daniel_plaster@live.de>
2022-05-08 04:02:37 +0200

    removed extra usings

--- Source/1.3/Windows/SettlementPlacementWindow.cs
+++ Source/1.3/Windows/SettlementPlacementWindow.cs
@@ -6,8 +6,6 @@ using Empire_Rewritten.Utils;
 using HarmonyLib;
 using RimWorld;
 using RimWorld.Planet;
-using RimWorld;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
