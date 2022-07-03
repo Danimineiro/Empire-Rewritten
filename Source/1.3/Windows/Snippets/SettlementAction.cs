2bd171742c2be5c0abe0eb02d3696753902935fb
Danimineiro <daniel_plaster@live.de>
2022-07-03 03:54:17 +0200

    Create SettlementAction.cs

---
+++ Source/1.3/Windows/Snippets/SettlementAction.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld.Planet;
+using System;
+using Verse;
+
+namespace Empire_Rewritten.Windows.Snippets
+{
+    public class SettlementAction
+    {
+        private string label;
+        private Action<Settlement> action;
+        private string labelCapCached;
+
+        public Action<Settlement> Action
+        {
+            get => action;
+            set => action = value;
+        }
+
+        public string Label
+        {
+            get => label;
+            set => label = value;
+        }
+
+        public string LabelCap
+        {
+            get => labelCapCached ?? (labelCapCached = label.CapitalizeFirst());
+            set => label = value;
+        }
+    }
+}
