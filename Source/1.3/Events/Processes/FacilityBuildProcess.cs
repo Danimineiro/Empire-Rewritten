0fe5b5018d98afb9fdb7e7d4da00ac0d19df0605
Danimineiro <daniel_plaster@live.de>
2022-07-04 05:39:24 +0200

    Create FacilityBuildProcess.cs

---
+++ Source/1.3/Events/Processes/FacilityBuildProcess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Empire_Rewritten.Facilities;
+using Verse;
+
+namespace Empire_Rewritten.Events.Processes
+{
+    internal class FacilityBuildProcess : Process
+    {
+        private FacilityManager facilityManager;
+        private FacilityDef facilityDef;
+        private int slotID;
+
+        public FacilityBuildProcess() { }
+
+        public FacilityBuildProcess(string label, string toolTip, int duration, FacilityManager facilityManager, FacilityDef facilityDef, int slotID) : base(label, toolTip, duration) 
+        { 
+            this.facilityManager = facilityManager; 
+            this.facilityDef = facilityDef;
+            this.slotID = slotID;
+        }
+
+        protected override object[] Parms => new object[] { facilityDef, facilityManager };
+
+        public int SlotID => slotID;
+
+        protected override void Run()
+        {
+            if (suspended) return;
+            Run(facilityDef, facilityManager);
+            facilityManager.NotifyProcessCompleted();
+        }
+
+        public static void Run(FacilityDef facilityDef, FacilityManager facilityManager)
+        {
+            if (facilityManager[facilityDef] is Facility facility)
+            {
+                facility.AddFacility();
+            }
+            else
+            {
+                facilityManager[facilityDef] = new Facility(facilityDef, facilityManager.Settlement);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_References.Look(ref facilityManager, nameof(facilityManager));
+            Scribe_Defs.Look(ref facilityDef, nameof(facilityDef));
+            Scribe_Values.Look(ref slotID, nameof(slotID));
+            base.ExposeData();
+        }
+    }
+}
