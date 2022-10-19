83f57f3046f75464317d99bf5eb024523053425c
Tobias Berger <tobi.berger13@gmail.com>
2022-10-19 20:18:34 +0200

    Refresh FacilityManager cache when Facility finishes building

--- Source/1.4/Events/Processes/FacilityBuildProcess.cs
+++ Source/1.4/Events/Processes/FacilityBuildProcess.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Empire_Rewritten.Facilities;
+﻿using Empire_Rewritten.Facilities;
 using Verse;
 
 namespace Empire_Rewritten.Events.Processes
 {
     /// <summary>
-    ///     A type of <see cref="Process"/> used to construct a new <see cref="Facility"/> inside a <see cref="FacilityManager"/>
+    ///     A type of <see cref="Process" /> used to construct a new <see cref="Facility" /> inside a
+    ///     <see cref="FacilityManager" />
     /// </summary>
     internal class FacilityBuildProcess : Process, IProcessSlotID
     {
-        private FacilityManager facilityManager;
         private FacilityDef facilityDef;
+        private FacilityManager facilityManager;
         private int slotID;
 
         /// <summary>
         ///     Used for saving/loading
         /// </summary>
-        public FacilityBuildProcess() : base() { }
+        public FacilityBuildProcess()
+        {
+        }
 
         /// <summary>
-        ///     Creates a new <see cref="FacilityBuildProcess"/>
+        ///     Creates a new <see cref="FacilityBuildProcess" />
         /// </summary>
         /// <param name="label"></param>
         /// <param name="toolTip"></param>
@@ -32,20 +30,23 @@ namespace Empire_Rewritten.Events.Processes
         /// <param name="facilityManager"></param>
         /// <param name="facilityDef"></param>
         /// <param name="slotID"></param>
-        public FacilityBuildProcess(string label, string toolTip, int duration, string iconPath, FacilityManager facilityManager, FacilityDef facilityDef, int slotID) : base(label, toolTip, duration, iconPath) 
-        { 
-            this.facilityManager = facilityManager; 
+        public FacilityBuildProcess(string label, string toolTip, int duration, string iconPath, FacilityManager facilityManager, FacilityDef facilityDef, int slotID) : base(label, toolTip, duration, iconPath)
+        {
+            this.facilityManager = facilityManager;
             this.facilityDef = facilityDef;
             this.slotID = slotID;
         }
 
         /// <summary>
-        ///     A number marking in which slot this <see cref="FacilityBuildProcess"/> works in
+        ///     A number marking in which slot this <see cref="FacilityBuildProcess" /> works in
         /// </summary>
         public int SlotID { get => slotID; set => slotID = value; }
 
         /// <summary>
-        ///     Overrides <see cref="Process.Run"/>; Builds a new <see cref="Facility"/>, or adds to it using <see cref="Facility.AddFacility"/> if it already exists, inside the given <see cref="FacilityManager"/> <paramref name="facilityManager"/> using the <see cref="FacilityDef"/> <paramref name="facilityDef"/>; Notifies the <see cref="facilityManager"/>
+        ///     Overrides <see cref="Process.Run" />; Builds a new <see cref="Facility" />, or adds to it using
+        ///     <see cref="Facility.AddFacility" /> if it already exists, inside the given <see cref="FacilityManager" />
+        ///     <paramref name="facilityManager" /> using the <see cref="FacilityDef" /> <paramref name="facilityDef" />; Notifies
+        ///     the <see cref="facilityManager" />
         /// </summary>
         protected override void Run()
         {
@@ -57,6 +58,7 @@ namespace Empire_Rewritten.Events.Processes
             {
                 facilityManager[facilityDef] = new Facility(facilityDef, facilityManager.Settlement);
             }
+            facilityManager.RefreshCaches();
             facilityManager.NotifyProcessesChanged();
         }
 
