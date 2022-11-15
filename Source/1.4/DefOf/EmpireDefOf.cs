7f5c19d044a63ab9ff7d6015770f2107777d1175
Danimineiro <daniel_plaster@live.de>
2022-11-16 00:22:42 +0100

    implements #40

---
+++ Source/1.4/DefOf/EmpireDefOf.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Empire_Rewritten.DefOf
+{
+    public static class EmpireThingDefOf
+    {
+        static EmpireThingDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+
+        public static ThingDef EmpireTaxSpot;
+    }
+}
