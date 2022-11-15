30c0c9b84d6fc0264245108c01e9280fb7b8f2eb
Danimineiro <daniel_plaster@live.de>
2022-11-16 00:24:51 +0100

    Cleanup + Util Class

---
+++ Source/1.4/Deliveries/DeliveryUtil.cs
@@ -0,0 +1,15 @@
+﻿using Empire_Rewritten.DefOf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Empire_Rewritten.Deliveries
+{
+    public static class DeliveryUtil
+    {
+        public static IntVec3? TryGetTaxSpotLocation(Map map) => map.listerThings.ThingsOfDef(EmpireThingDefOf.EmpireTaxSpot).FirstOrFallback()?.Position;
+    }
+}
